1dbee15eeacfba56916de2c831eb8be6b8bb59fa
Michael Downey <mdowney17@qub.ac.ukcd>
2019-06-27 09:28:31 +0100

    Renamed navbar and fixed a small bug

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 // Stateless Functional Component
 const NavBar = ({ totalCounters }) => {
@@ -7,7 +7,7 @@ const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="/">
-        Navbar{" "}
+        Shopping Cart{" "}
         <span className="badge badge-pill badge-secondary">
           {totalCounters}
         </span>
